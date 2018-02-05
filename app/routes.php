53db0720a0058c060fff8335587e1a652b9033db
Michael <michaelthomas372@gmail.com>
2018-02-05 01:45:53 +0100

    added guest middleware

--- app/routes.php
+++ app/routes.php
@@ -4,14 +4,16 @@ use App\Controllers\HomeController;
 use App\Controllers\Auth\AuthController;
 use App\Controllers\Auth\PasswordController;
 use App\Middleware\AuthenticatedMiddleware;
+use App\Middleware\GuestMiddleware;
 
 $app->get('/', HomeController::class . ':index')->setName('home');
 
-$app->get('/auth/signup', AuthController::class . ':getSignUp')->setName('auth.signup');
-$app->post('/auth/signup', AuthController::class . ':postSignUp');
-
-$app->get('/auth/signin', AuthController::class . ':getSignIn')->setName('auth.signin');
-$app->post('/auth/signin', AuthController::class . ':postSignIn');
+$app->group('', function(){
+    $this->get('/auth/signup', AuthController::class . ':getSignUp')->setName('auth.signup');
+    $this->post('/auth/signup', AuthController::class . ':postSignUp');
+    $this->get('/auth/signin', AuthController::class . ':getSignIn')->setName('auth.signin');
+    $this->post('/auth/signin', AuthController::class . ':postSignIn');
+})->add(new GuestMiddleware($container));
 
 $app->group('', function(){
     $this->get('/auth/signout', AuthController::class . ':getSignOut')->setName('auth.signout');
